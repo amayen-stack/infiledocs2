93547079ced52fc3ac163f667596335ea1c133a8
amayen-stack <amayen@infile.com>
2026-03-24 17:05:10 -0600

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -6,10 +6,6 @@ const config: DocsThemeConfig = {
   project: {
     link: 'https://infile.com/hubfs/fav%20icon%201%20infile.png',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
     text: 'Infile, S.A.',
   },
